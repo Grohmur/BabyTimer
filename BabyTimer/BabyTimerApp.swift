086a18a0cdde6392e42e800da24fc2e868ccd494
Grohmur <zverma92@gmail.com>
2022-03-13 10:49:51 +0300

    Initial Commit

---
+++ BabyTimer/BabyTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BabyTimerApp.swift
+//  BabyTimer
+//
+//  Created by Михаил Зверьков on 13.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct BabyTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
